56cbc5ada948ee1daee5259f34dbdbf0a5ff6690
Ranbir <mahtanir@wharton.upenn.edu>
2022-03-17 03:14:19 -0400

    idk what this is please verify check statements

--- smart-contract/deploy.py
+++ smart-contract/deploy.py
@@ -1,31 +1,64 @@
 from algosdk.future import transaction
 from algosdk import account, mnemonic
-from secrets import account_mnemonics
-from election_params import local_ints, local_bytes, global_ints, \
-    global_bytes
+from algosdk.v2client import algod
+from secrets import account_mnemonics, algod_address, algod_headers
+from election_params import local_ints, local_bytes, global_ints, global_bytes, relative_election_end, num_vote_options, vote_options #CHECK THIS
+from helper import compile_program, read_global_state
+from pyteal import compileTeal, Mode
+from election_smart_contract import (
+    clear_state_program,
+    approval_program,
+)  # CHECK IMPORT
 
 # Define keys, addresses, and token
 account_private_keys = [mnemonic.to_private_key(mn) for mn in account_mnemonics]
-account_addresses = [account.address_from_private_key(sk) for sk in account_private_keys]
+account_addresses = [
+    account.address_from_private_key(sk) for sk in account_private_keys
+]
 
 # Declare application state storage for local and global schema
 global_schema = transaction.StateSchema(global_ints, global_bytes)
 local_schema = transaction.StateSchema(local_ints, local_bytes)
 
 
-def create_app(client, private_key, approval_program, clear_program, global_schema, local_schema, app_args):
+def create_app(
+    client,
+    private_key,
+    approval_program,
+    clear_program,
+    global_schema,
+    local_schema,
+    app_args,
+):
     """
     Create a new application from the compiled approval_program, clear_program
     using the application arguments app_args
     Return the newly created application ID
     """
     # TODO: define sender as creator
+    sender = account.address_from_private_key(private_key)
     # TODO: declare the on_complete transaction as a NoOp transaction
+    on_complete = transaction.OnComplete.NoOpOC.real
     # TODO: get node suggested parameters
+    params = client.suggested_params()
     # TODO: create unsigned transaction
+    txn = transaction.ApplicationCreateTxn(
+        sender,
+        params,
+        on_complete,
+        approval_program,
+        clear_program,
+        global_schema,
+        local_schema,
+        app_args,
+    )
     # TODO: sign transaction
+    signed_txn = txn.sign(private_key)
+    tx_id = signed_txn.transaction.get_txid()
     # TODO: send transaction
+    client.send_transactions([signed_txn])
     # TODO: await confirmation
+    wait_for_confirmation(client, tx_id)
 
     # display results
     transaction_response = client.pending_transaction_info(tx_id)
@@ -34,27 +67,72 @@ def create_app(client, private_key, approval_program, clear_program, global_sche
 
     return app_id
 
+def intToBytes(i):
+    return i.to_bytes(8, "big")
 
-def create_vote_app(client, creator_private_key, election_end, num_vote_options, vote_options):
+def create_vote_app(
+    client, creator_private_key, election_end, num_vote_options, vote_options
+):
     """
     Create/Deploy the voting app
     This function uses create_app and return the newly created application ID
     """
-    # TODO:
-    # Get PyTeal approval program
+    # get PyTeal approval program
+    approval_program_ast = approval_program()
     # compile program to TEAL assembly
+    approval_program_teal = compileTeal(
+        approval_program_ast, mode=Mode.Application, version=5
+    )
     # compile program to binary
-    # Do the same for PyTeal clear state program
-    # TODO: Create list of bytes for application arguments and create new application. 
+    approval_program_compiled = compile_program(client, approval_program_teal) #CHECK: client is this right
 
-    app_id = None
+    # Do the same for PyTeal clear state program
+
+    # get PyTeal clear state program
+    clear_state_program_ast = clear_state_program()
+    # compile program to TEAL assembly
+    clear_state_program_teal = compileTeal(
+        clear_state_program_ast, mode=Mode.Application, version=5
+    )
+    # compile program to binary
+    clear_state_program_compiled = compile_program(
+        client, clear_state_program_teal
+    )
+
+    # TODO: Create list of bytes for application arguments and create new application.
+     app_args = [
+        intToBytes(election_end),
+        intToBytes(num_vote_options),
+        Bytes(num_vote_options) #CHECK should I enclose with Bytes()  or no need
+    ]
+
+    # create new application
+    app_id = create_app(
+        algod_client,
+        creator_private_key,
+        approval_program_compiled,
+        clear_state_program_compiled,
+        global_schema,
+        local_schema,
+        app_args,
+    )
 
     return app_id
 
 
 def main():
     # TODO: Initialize algod client and define absolute election end time fom the status of the last round.
+    # initialize an algodClient
+    algod_client = algod.AlgodClient(algod_headers["X-API-Key"], algod_address)
+    # configure registration and voting period
+    status = algod_client.status()
+    election_end = status["last-round"] + relative_election_end
+     # read global state of application
+
     # TODO: Deploy the app and print the global state.
+    app_id = create_vote_app(algod_client, account_private_keys[0], election_end, num_vote_options, vote_options) 
+    #is this right CHECK for params and does this constitue deploying or do I need all the other functions also very confused
+    print("Global state:", read_global_state(algod_client, app_id))
 
     pass
 
--- smart-contract/election_smart_contract.py
+++ smart-contract/election_smart_contract.py
@@ -6,21 +6,27 @@ def approval_program():
     """APPROVAL PROGRAM handles the main logic of the application"""
 
     i = ScratchVar(TealType.uint64)  # i-variable for for-loop
+    prevSet = ScratchVar(TealType.uint64)
 
-    on_creation = Seq(
+    on_creation = Seq( #HOW TO: SKIP COMMAS i.e only works for single legnth vote 
         [
             # TODO:
             # Check number of required arguments are present
+            Assert(Txn.application_args.length() == Int(3)),
             # Store relevant parameters of the election. When storing the options to vote for,
+            App.globalPut(Bytes("ElectionEnd"), Global.round() + Btoi(Txn.application_args[0])),
+            App.globalPut(Bytes("NumVoteOptions"), Btoi(Txn.application_args[1])),
+            App.globalPut(Bytes("VoteOptions"), Bytes(Txn.application_args[2])), #CHECK THIS --> necessary? or no need? 
             # consider storing all of them as a string separated by commas e.g: "A,B,C,D".
             # Note that index-wise, A=0, B=1, C=2, D=3
             # Set all initial vote tallies to 0 for all vote options, keys are the vote options
+            prevSet.store(Int(0))
             For(
-
+                i.store(Int(0)),
+                i.load() < Btoi(Txn.application_args[1]),
+                i.store(i.load() + Int(1)),
             ).Do(
-
-            ),
-
+                App.globalPut(Concat(Bytes("VotesFor"), itoa(i)), 0)),
             Return(Int(1)),
         ]
     )
@@ -37,52 +43,97 @@ def approval_program():
     # If a user wants to vote for C, then the choice that the user wants to vote for is equivalent to the uint 2
     get_vote_of_sender = App.localGetEx(Int(0), App.id(), Bytes("voted"))
 
+    sender_can_vote_update = App.localGetEx(Txn.application_args[1], App.id(), Bytes("can_vote"))
+
     on_closeout = Seq(
         # TODO: CLOSE OUT:
         [
-
-            Return(Int(1))
+            get_vote_of_sender,
+            # if statement
+            If(
+                And(
+                    Global.round() <= App.globalGet(Bytes("ElectionEnd")),
+                    get_vote_of_sender.hasValue(), #alr voted so must be included > invariant 
+                ),
+                App.globalPut(
+                    get_vote_of_sender.value(),
+                    App.globalGet(get_vote_of_sender.value()) - Int(1)
+                ),
+            ),
+            Return(Int(1)),
         ]
     )
 
     on_register = Seq(
-        # TODO: REGISTRATION:
-        Return(Int(1))
+        [
+            Assert(Global.round() <= App.globalGet(Bytes("ElectionEnd"))),
+            App.localPut(Int(0), Bytes("can_vote"), Bytes("maybe")),#CHECK Int(0) or Txn.sender()??
+            Return(Int(1)),
+        ]
     )
 
-    on_update_user_status = Seq(
-        # TODO: UPDATE USER LOGIC
+    on_update_user_status = Seq( #CHECK sender_can_vote_update
+       [
+           #get_sender_can_vote,
+           sender_can_vote_update,
+           Assert(
+                And(
+                    Global.round() <= App.globalGet(Bytes("ElectionEnd")),
+                    is_creator,
+                    sender_can_vote_update.hasValue(), #if it doesn't not registered
+                    sender_can_vote_update.value() != "yes",
+                    sender_can_vote_update.value() != "no", #else alr updated
+                )
+            ),
+           App.localPut(Txn.application_args[1], Bytes("can_vote"), Txn.application_args[2])
+        ]
         Return(Int(1))
     )
 
     choice = Btoi(Txn.application_args[1])
+    choice_tally = App.globalGet(choice)
     on_vote = Seq(
-        # TODO: USER VOTING LOGIC:
         [
-
-            Return(Int(1))
-        ]
+            Assert(
+                And(
+                    Global.round() <= App.globalGet(Bytes("ElectionEnd")),
+                    get_sender_can_vote.hasValue(),
+                    get_sender_can_vote.value() == "yes"
+                )
+            ), 
+            get_vote_of_sender,
+            If(get_vote_of_sender.hasValue(), Return(Int(0))),
+            Assert(
+                And(
+                    choice >= 0,
+                    choice <= App.globalGet(Bytes("NumVoteOptions")),
+                )
+            ),
+            App.globalPut(choice, choice_tally + Int(1)),
+            App.localPut(Int(0), Bytes("voted"), choice), #Int(0) refers to present sender address
+            Return(Int(1))]
     )
 
     program = Cond(
-
         # MAIN CONDITIONAL
-
         [Txn.application_id() == Int(0), on_creation],
         [Txn.on_completion() == OnComplete.DeleteApplication, Return(is_creator)],
         [Txn.on_completion() == OnComplete.UpdateApplication, Return(is_creator)],
         [Txn.on_completion() == OnComplete.CloseOut, on_closeout],
         [Txn.on_completion() == OnComplete.OptIn, on_register],
-
+        [Txn.application_args[0] == Bytes("vote"), on_vote],
+        [
+            Txn.application_args[0] == Bytes("update_user_status"),
+            on_update_user_status,
+        ]  # CHECK!
         # TODO: Complete the cases that will trigger the update_user_status and on_vote sequences
-
     )
 
     return program
 
 
 def clear_state_program():
-    """ Handles the logic of when an account clears its participation in a smart contract. """
+    """Handles the logic of when an account clears its participation in a smart contract."""
 
     # TODO: CLEAR STATE PROGRAM
 
@@ -91,8 +142,19 @@ def clear_state_program():
     program = Seq(
         # remove their vote from the correct vote tally
         [
-
-            Return(Int(1))
+            get_vote_of_sender,
+            # if statement
+            If(
+                And(
+                    Global.round() <= App.globalGet(Bytes("ElectionEnd")),
+                    get_vote_of_sender.hasValue(), #alr voted so must be included > invariant 
+                ),
+                App.globalPut(
+                    get_vote_of_sender.value(),
+                    App.globalGet(get_vote_of_sender.value()) - Int(1)
+                ),
+            ),
+            Return(Int(1)),
         ]
     )
 
@@ -100,7 +162,7 @@ def clear_state_program():
 
 
 if __name__ == "__main__":
-    
+
     with open("vote_approval.teal", "w") as f:
         compiled = compileTeal(approval_program(), mode=Mode.Application, version=5)
         f.write(compiled)
